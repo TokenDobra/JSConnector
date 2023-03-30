0223743fe2b827a34273016f795a8a719c400918
Konstantin Golovkin <kgolovkin@mail.ru>
2023-03-31 02:21:36 +0300

    Работа с корзиной

--- assets/js/tokendobra_cart.js
+++ assets/js/tokendobra_cart.js
@@ -1,14 +1,14 @@
-
-const loadContent = async() =>
-{
-  const offers = await berpSDK.api.getOffers();
-  if(!offers.length)
-    return;
-
-}
-
+const 
 $(document).ready(function(){
 //   loadContent();
-console.log('tcart', window.tcart); 
+
+  console.log('tcart', window.tcart); 
+  window.tcart.products = [];
+  window.tcart.prodamount = 0;
+  window.tcart.amount = 0;
+  window.tcart.total = 0;
+  tcart__updateTotalProductsinCartObj();
+  console.log('tcart', window.tcart); 
+
 });
           
